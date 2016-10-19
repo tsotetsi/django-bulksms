554d25741609556bb4844301deeadc310dffb530
Thapelo <me@tsotetsithapelo.co.za>
2016-10-19 08:24:33 +0200

    Move source files to package directory

---
+++ bulksms/__init__.py
@@ -0,0 +1,12 @@
+"""
+django-bulksms library
+-------------------------------------------------------------------------------
+
+Several libraries for sending bulk sms.
+:license: MIT. See LICENSE for more details
+"""
+
+__title__ = 'django-bulksms'
+__version__ = '0.0.1'
+__author__ = 'Thapelo Tsotetsi'
+__license__ = 'MIT'
