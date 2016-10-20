b759f7bc93cd5e38c1b7a09536331f9a69ea7b4f
Thapelo <me@tsotetsithapelo.co.za>
2016-10-20 08:01:13 +0200

    Move files to src folder and fix tox issue nnot finding a module when importing.

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-#!/usr/bin/env python
-import os
 from setuptools import setup
 
-import bulksms
-
-
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup to be run from any path.
-
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 CLASSIFIERS = [
     'Programming Language :: Python',
@@ -38,8 +27,8 @@ setup(
     name='django-bulksms',
     description='Django BulkSMS library/API.',
     long_description='Django BulkSMS library/API',
-    version=bulksms.__version__,
-    package_dir={'bulksms': ''},
+    version='0.0.1',
+    package_dir={'': 'src'},
     packages=['bulksms'],
     include_package_data=True,
     zip_safe=False,
