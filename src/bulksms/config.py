cf3168f196d632dd34cb54af24e65b84b3b5806f
Thapelo <me@tsotetsithapelo.co.za>
2017-01-23 16:38:02 +0200

    Refactor config file to a DICT for easy referencing

--- src/bulksms/config.py
+++ src/bulksms/config.py
@@ -1,4 +1,14 @@
-url = "https://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0"
-url_batch = "http://bulksms.2way.co.za/eapi/submission/send_batch/1/1.0"
-username = "<USERNAME>"
-password = "<PASSWORD>"
+# BULKSMS Config.
+CONFIG = {
+    'BULK_SMS': {
+        'AUTH': {
+            'USERNAME': '',
+            'PASSWORD': ''
+        },
+        'URL': {
+            'SINGLE': 'https://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0',
+            'BATCH': 'http://bulksms.2way.co.za/eapi/submission/send_batch/1/1.0'
+        }
+    },
+    'CLEAN_MOBILE_NUMBERS': False
+}
