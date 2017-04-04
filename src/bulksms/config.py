62eea0104615b5d75183d5392fe250fa07d2a988
Thapelo <me@tsotetsithapelo.co.za>
2017-04-04 16:23:20 +0200

    Add url for getting credits.

--- src/bulksms/config.py
+++ src/bulksms/config.py
@@ -6,8 +6,15 @@ CONFIG = {
             'PASSWORD': ''
         },
         'URL': {
-            'SINGLE': 'https://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0',
-            'BATCH': 'http://bulksms.2way.co.za/eapi/submission/send_batch/1/1.0'
+            'SENDING': {
+                {
+                    'SINGLE': 'https://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0',
+                    'BATCH': 'https://bulksms.2way.co.za/eapi/submission/send_batch/1/1.0'
+                }
+            },
+            'CREDITS': {
+                'CREDITS': 'https://bulksms.2way.co.za/user/get_credits/1/1.1'
+            }
         }
     },
     'CLEAN_MOBILE_NUMBERS': False
