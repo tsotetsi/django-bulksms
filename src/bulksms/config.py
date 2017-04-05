f0d08f01ed65024b2b6aa8bcdda99eb9aa4e3272
Thapelo <me@tsotetsithapelo.co.za>
2017-04-05 08:19:56 +0200

    Refactor config file. Nested dictionary was a bad idea.

--- src/bulksms/config.py
+++ src/bulksms/config.py
@@ -1,23 +1,21 @@
-# BULKSMS Config.
-CONFIG = {
-    'BULK_SMS': {
-        'AUTH': {
-            'USERNAME': '',
-            'PASSWORD': ''
-        },
-        'URL': {
-            'SENDING': {
-                {
-                    'BATCH': 'https://bulksms.2way.co.za/eapi/submission/send_batch/1/1.0'
-                },
-                {
-                    'SINGLE': 'https://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0'
-                }
-            },
-            'CREDITS': {
-                'CREDITS': 'https://bulksms.2way.co.za/user/get_credits/1/1.1'
-            }
-        }
-    },
-    'CLEAN_MOBILE_NUMBERS': False
+# BULKSMS Configuration.
+
+# Credentials required to use bulk sms api.
+AUTH = {
+    'username': '',
+    'password': ''
 }
+# URL for sending single and batch sms.
+URL_SENDING = {
+    'batch': 'https://bulksms.2way.co.za/eapi/submission/send_batch/1/1.0',
+    'single': 'https://bulksms.2way.co.za/eapi/submission/send_sms/2/2.0'
+}
+
+# URL for fetching credits related data.
+URL_STATS = {
+    'credits': 'https://bulksms.2way.co.za/user/get_credits/1/1.1'
+}
+
+# Whether to automatically insert country
+# codes before sending sms.
+CLEAN_MOBILE_NUMBERS = False
